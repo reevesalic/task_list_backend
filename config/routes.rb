4f8ee3e31cbd4f63860c01f154582fbf3f3803fe
reevesalic <reevesalic@gmail.com>
2021-02-02 14:57:52 -0500

    built out category controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
-  resources :tasks
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+end
 end
