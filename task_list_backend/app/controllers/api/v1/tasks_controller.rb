e9aae0607f2547d214d5339a1a183ccf2c7232fb
reevesalic <reevesalic@gmail.com>
2021-02-03 12:00:43 -0500

    added serializer

---
+++ task_list_backend/app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::TasksController < ApplicationController
+
+     def index
+          @tasks = Task.all
+          render json: TaskSerializer.new(@tasks)
+     end
+
+     def show
+          @task = Task.find(params[:id])
+          render json: @task, status: 200
+     end
+
+     def create
+          @task = Task.create(task_params)
+          render json: @task, status: 200
+     end
+
+     def update
+          @task = Task.find(params[:id])
+          @task.update(task_params)
+          render json: @task, status: 200
+     end
+
+     def destroy
+          @task = Task.find(params[:id])
+          @task.delete
+          render json: {taskId: @task.id}
+     end
+     private
+     def task_params
+          params.require(:task).permit(:task, :phone_number, :category_id)
+     end
+end
+
